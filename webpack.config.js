109956a799192339b82038221c60045fc4f0977a
Jordan <jclemens24@hotmail.com>
2022-08-16 21:58:09 -0400

    remove clean dist option from webpack config so assets stay

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    clean: true,
+    clean: false,
     assetModuleFilename: 'images/[hash][ext]',
   },
   optimization: {
